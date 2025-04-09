0f48cd8ef782a78b5b5456cd8c07ca5a349a29ff
Iryna Svyrydenko <svyrydenko.pn@edu.ucu.ua>
2025-04-09 10:40:08 +0300

    implementation of stack using two queues

--- implement_stack_using_queues/implement_stack_using_queues.py
+++ implement_stack_using_queues/implement_stack_using_queues.py
@@ -2,36 +2,41 @@ from collections import deque
 class MyStack(object):
 
     def __init__(self):
-        self.q = deque()
-        # self.q2 = deque()
+        self.q1 = deque()
+        self.q2 = deque()
 
     def push(self, x):
         """
         :type x: int
         :rtype: None
         """
-        self.q.append(x)
+        self.q1.append(x)
 
     def pop(self):
         """
         :rtype: int
         """
-        for i in range(len(self.q)-1):
-            self.push(self.q.popleft())
-        return self.q.popleft()
+        if self.q1:
+            for i in range(len(self.q1)-1):
+                self.q2.append(self.q1.popleft())
+            return self.q1.popleft()
+        for i in range(len(self.q2)-1):
+            self.q1.append(self.q2.popleft())
+        return self.q2.popleft()
 
     def top(self):
         """
         :rtype: int
         """
-        return self.q[-1]
+        if not self.q1:
+            return self.q2[-1]
+        return self.q1[-1]
 
     def empty(self):
         """
         :rtype: bool
         """
-        return not self.q
-
+        return not self.q1 and not self.q2
 
 # Your MyStack object will be instantiated and called as such:
 # obj = MyStack()
